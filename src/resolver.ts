90f7f10a0303b57f6dd436f7db63f3d2ce69bca6
Ilan Olkies <ilanolkies@outlook.com>
2021-03-16 21:37:52 -0300

    Refactor rpc request

--- src/resolver.ts
+++ src/resolver.ts
@@ -20,45 +20,34 @@ export class Resolver {
     this.fetch = options.fetch ?? fetch
   }
 
+  private rpcRequest = (params: (string | { [key: string]: string })[]) => this.fetch(this.rpcUrl, {
+    method: 'post',
+    body: JSON.stringify({
+      jsonrpc: '2.0',
+      method: 'eth_call',
+      id: 666,
+      params
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  }).then((res: Response | NodeFetchResponse) => res.json())
+    .then(({ result, error, id }) => {
+      if (id !== 666) throw new Error('Invalid RPC response: id mismatch')
+      if (error) throw new Error('RPC Call error: ' + error)
+      return result
+    })
+
   async addr(domain: string): Promise<string> {
     const node = namehash(domain)
 
-    const resolverAddress = await this.fetch(this.rpcUrl, {
-      method: 'post',
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method: 'eth_call',
-        id: 666,
-        params: [{ to: this.registryAddress, data: '0x0178b8bf' + node.slice(2) }]
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    }).then((res: Response | NodeFetchResponse) => res.json())
-      .then(({ result, error, id }) => {
-        if (id !== 666) throw new Error('Invalid RPC response: id mismatch')
-        if (error) throw new Error('RPC Call error: ' + error)
-        return result
-      })
-      .then(result => '0x' + result.slice(-40))
+    const resolverAddress = await this.rpcRequest(
+      [{ to: this.registryAddress, data: '0x0178b8bf' + node.slice(2) }]
+    ).then(result => '0x' + result.slice(-40))
 
     if (resolverAddress === '0x0000000000000000000000000000000000000000') throw new Error('Domain has no resolver')
 
-
-    const supportsAddr = await this.fetch(this.rpcUrl, {
-      method: 'post',
-      body: JSON.stringify({
-        jsonrpc: '2.0',
-        method: 'eth_call',
-        id: 666,
-        params: [{ to: resolverAddress, data: '0x01ffc9a73b3b57de00000000000000000000000000000000000000000000000000000000' }]
-      }),
-      headers: { 'Content-Type': 'application/json' },
-    }).then((res: Response | NodeFetchResponse) => res.json())
-      .then(({ result, error, id }) => {
-        if (id !== 666) throw new Error('Invalid RPC response: id mismatch')
-        if (error) throw new Error('RPC Call error: ' + error)
-        return result
-      })
-      .then(result => result !== '0x')
+    const supportsAddr = await this.rpcRequest(
+      [{ to: resolverAddress, data: '0x01ffc9a73b3b57de00000000000000000000000000000000000000000000000000000000' }]
+    ).then(result => result !== '0x')
 
     if(!supportsAddr) throw new Error('Domain has no addr resolver')
 
